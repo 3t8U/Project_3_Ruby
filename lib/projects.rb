a82e4604b5c628014c80fa3f2bbcd2cc46309d65
Student <example@example.com>
2020-01-10 09:48:04 -0800

    continue to build infrastructure

---
+++ lib/projects.rb
@@ -0,0 +1,109 @@
+class Project
+
+  attr_reader :id, :title, :genre
+
+  def initialize(attributes)
+    @title = attributes.fetch(:title)
+    @id = attributes.fetch(:id)
+    @genre = attributes.fetch(:genre)
+  end
+
+  def ==(project_to_compare)
+    @id == project_to_compare.id && @title == project_to_compare.title
+  end
+
+  def Project.clean(string)
+    (string.include?("'")) ? string.gsub("'", "''") : string
+  end
+
+  def save
+    result = DB.exec("
+      INSERT INTO projects (title, genre)
+      VALUES ('#{Project.clean(@title)}', '#{@genre}') RETURNING id;
+    ")
+    @id = result.first().fetch('id').to_i
+  end
+
+  def self.get_projects(db_query)
+    projects_array = []
+    db_query_results = DB.exec(db_query)
+    db_query_results.each do |project|
+      id = project.fetch('id').to_i
+      title = project.fetch('title')
+      genre = project.fetch('genre')
+      projects_array.push(Project.new({ :id => id, :title => title, :genre => genre }))
+    end
+    projects_array
+  end
+
+  def self.all
+    Project.get_projects("SELECT * FROM projects;")
+  end
+
+  def delete
+    DB.exec("DELETE FROM projects WHERE id = #{@id};")
+    DB.exec("DELETE FROM projects_volunteers WHERE project_id = #{@id};")
+  end
+
+  def self.find(id)
+    Project.get_projects("SELECT * FROM projects WHERE id = #{id};").first()
+  end
+
+  def self.search(search)
+    if search.fetch(:title) != ''
+      Project.get_projects("
+        SELECT * FROM projects
+        WHERE lower(title) LIKE '%#{self.clean(search.fetch(:title).downcase)}%';
+      ")
+    elsif search.fetch(:genre) != ''
+      Project.get_projects("
+        SELECT * FROM projects
+        WHERE genre = '#{search.fetch(:genre)}';
+      ")
+    end
+  end
+
+  def update(attributes)
+    @title = (attributes.fetch(:title) && attributes.fetch(:title) != '') ?
+      attributes.fetch(:title) :
+      @title
+    @genre = (attributes.fetch(:genre) && attributes.fetch(:genre) != '') ?
+      attributes.fetch(:genre) :
+      @genre
+    DB.exec("
+      UPDATE projects SET title = '#{Project.clean(@title)}', genre = '#{@genre}'
+      WHERE id = #{@id};
+    ")
+  end
+
+  def add_volunteer(title)
+    result = Volunteer.get_volunteers("
+      SELECT * FROM volunteers WHERE title = '#{Project.clean(title)}'
+    ")
+    if(result.length <1)
+      volunteer = Volunteer.new({
+        :id => nil,
+        :title => "#{title}"
+      })
+      volunteer.save()
+      result = [volunteer]
+    end
+    DB.exec("
+      INSERT INTO projects_volunteers (volunteer_id, project_id)
+      VALUES (#{result.first().id}, #{@id})
+    ")
+  end
+
+  def volunteers
+    results = DB.exec("SELECT * FROM projects_volunteers WHERE project_id = #{@id}")
+    id_string = results.map{ |result| result.fetch("volunteer_id")}.join(', ')
+    (id_string != '') ?
+      Volunteer.get_volunteers("SELECT * FROM volunteers WHERE id IN (#{id_string});") :
+      nil
+  end
+
+
+
+
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,16 @@
-require "volunteer"
-require "project"
-require "rspec"
-require "pry"
-require "pg"
+require 'rspec'
+require 'pg'
+require 'project'
+require 'volunteer'
+require 'pry'
+require './config'
 
-DB = PG.connect({:dbname => 'volunteer_tracker_test'})
+DB = PG.connect(TEST_DB_PARAMS)
 
 RSpec.configure do |config|
   config.after(:each) do
-    DB.exec('DELETE FROM volunteers *;')
-    DB.exec('DELETE FROM projects *;')
+    DB.exec("DELETE FROM projects *;")
+    DB.exec("DELETE FROM volunteers *;")
+    DB.exec("DELETE FROM projects_volunteers *;")
   end
 end
